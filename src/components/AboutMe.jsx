1c15fb2c0a2e70169fd0d10ba54cec4f38358f06
Patrick-B-L <patrick.brandt.lu@outlook.com>
2025-07-01 19:50:57 +0200

    Updates to HeroSection

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,6 +2,15 @@ import React from 'react'
 
 export default function AboutMe() {
   return (
-    <div id='AboutMe'>AboutMe</div>
+    <div id='AboutMe'>
+      <div>
+        <h2>About Me</h2>
+      </div>
+      <div>
+        <p>
+          I am a software developer with a passion for creating efficient and scalable applications. I have experience in various programming languages and frameworks, and I enjoy solving complex problems through code. In my free time, I like to contribute to open-source projects and stay updated with the latest trends in technology.
+        </p>
+      </div>
+    </div>
   )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,6 +2,19 @@ import React from 'react'
 
 export default function Projects() {
   return (
-    <div id='Projects'>Projects</div>
+    <div id='Projects'>
+      <div>
+        <h2>Projects</h2>     
+      </div>
+      <div className='project-tiles'>
+        <div className='project-tile'>
+          <h3>Project 1</h3>
+          <img src='' alt='Project 1' />
+          <p>Description of project 1. This project involves...</p>
+          <a href='' target='_blank' rel='noopener noreferrer'>View Project</a>
+        </div>
+      </div>
+      
+    </div>
   )
 }
