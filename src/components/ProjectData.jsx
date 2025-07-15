877e649f2de1c8c7a20e77e372f4dbaafb6ed5e0
Patrick-B-L <patrick.brandt.lu@outlook.com>
2025-07-15 16:07:08 +0200

    Minor updates

--- src/components/ProjectData.jsx
+++ src/components/ProjectData.jsx
@@ -1,11 +1,17 @@
 // This file contains an array of project data objects.// 
 const projectData = [
-  {image: '../images/Munamii.jpg', title: 'Munamii Cakery', description: "This is a frontend project showcasing a fictional bakery's products, including cupcakes and wedding cakes.", website: 'https://patrick-b-l.github.io/Munamii-Cakery/Home/index.html', ghLink: 'https://github.com/Patrick-B-L/Munamii-Cakery' }, 
-  {image: '../images/fim2.jpg', title: 'Film i Malmö (Team)', description: 'This project is a web application for Film i Malmö, providing a platform to explore movie schedules, upcoming screenings, and cinema-related content in Malmö. ', website: 'https://film-i-malmo.vercel.app/', ghLink: 'https://github.com/owenboy9/film-i-malmo' }, 
-  {image: '../images/bubblespin.jpg', title: 'BubbleSpin (Team)', description: 'BubbleSpin is a feature-rich React application designed to help users efficiently manage tasks, events, and schedules.', website: 'https://zereis.github.io/Event-planner/#/', ghLink: 'https://github.com/Zereis/Event-planner'},
-  {image: '../images/Todoly.jpg', title: 'Todoly', description: 'ToDoLy is a task management console application designed to help users organize and manage their tasks effectively. It provides functionality for creating, updating, sorting, and deleting tasks, with a user-friendly and modular design.', ghLink: 'https://github.com/Patrick-B-L/ToDoLy'},
-  {image: '../images/Todoapp.jpg', title: 'Todo app', description: 'Todo app is a React-based task manager with a clean design. Users can add tasks with deadlines, filter and sort them, mark as completed, and remove tasks. The app supports exporting and importing todos as JSON files, and saves data locally in the browser for persistent usage.', website: 'https://patrick-b-l.github.io/todo-app/', ghLink: 'https://github.com/Patrick-B-L/todo-app'},
-  {image: '../images/Assettracker.jpg', title: 'Asset Tracker', description: 'AssetTracker is a C#/.NET 8 console application for managing and tracking electronic assets (such as computers and phones) across different offices and countries. It supports exporting asset data, and uses Entity Framework Core with a SQL Server database.', ghLink: 'https://github.com/Patrick-B-L/AssetTracker'}
+
+  {image: process.env.PUBLIC_URL + '/images/Munamii.jpg', title: 'Munamii Cakery', description: "This is a frontend project showcasing a fictional bakery's products, including cupcakes and wedding cakes.", website: 'https://patrick-b-l.github.io/Munamii-Cakery/Home/index.html', ghLink: 'https://github.com/Patrick-B-L/Munamii-Cakery' },
+
+  {image: process.env.PUBLIC_URL + '/images/fim2.jpg', title: 'Film i Malmö (Team)', description: 'This project is a web application for Film i Malmö, providing a platform to explore movie schedules, upcoming screenings, and cinema-related content in Malmö. ', website: 'https://film-i-malmo.vercel.app/', ghLink: 'https://github.com/owenboy9/film-i-malmo' },
+
+  {image: process.env.PUBLIC_URL + '/images/bubblespin.jpg', title: 'BubbleSpin (Team)', description: 'BubbleSpin is a feature-rich React application designed to help users efficiently manage tasks, events, and schedules.', website: 'https://zereis.github.io/Event-planner/#/', ghLink: 'https://github.com/Zereis/Event-planner'},
+
+  {image: process.env.PUBLIC_URL + '/images/Todoly.jpg', title: 'Todoly', description: 'ToDoLy is a task management console application designed to help users organize and manage their tasks effectively. It provides functionality for creating, updating, sorting, and deleting tasks, with a user-friendly and modular design.', ghLink: 'https://github.com/Patrick-B-L/ToDoLy'},
+
+  {image: process.env.PUBLIC_URL + '/images/Todoapp.jpg', title: 'Todo app', description: 'Todo app is a React-based task manager with a clean design. Users can add tasks with deadlines, filter and sort them, mark as completed, and remove tasks. The app supports exporting and importing todos as JSON files, and saves data locally in the browser for persistent usage.', website: 'https://patrick-b-l.github.io/todo-app/', ghLink: 'https://github.com/Patrick-B-L/todo-app'},
+  
+  {image: process.env.PUBLIC_URL + '/images/Assettracker.jpg', title: 'Asset Tracker', description: 'AssetTracker is a C#/.NET 8 console application for managing and tracking electronic assets (such as computers and phones) across different offices and countries. It supports exporting asset data, and uses Entity Framework Core with a SQL Server database.', ghLink: 'https://github.com/Patrick-B-L/AssetTracker'}
 
 ];
 
